7809bb068a54f137ec7f6e8e43aeb52be354f389
Olufemi Dominic Fabian <olufmeidoominicfabian@yahoo.com>
2023-09-13 17:35:48 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,22 @@
-#include "main.h"
-
+#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * main - prints the sum of all multipples of 3 or 5 up to 1024
+ * Return: Always (Success)
  */
 
-
 int main(void)
-{
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
-}
 
+{
+	int i, z = 0;
+	while(i < 1024)
+	{
+	if ((i % 3 == 0) \\ (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printf("%d\n", z);
+	return(0);
+}
